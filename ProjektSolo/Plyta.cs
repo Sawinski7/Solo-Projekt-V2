4b92d68b943fb20c150c11fd0e34e3d7c72d7c13
Sawicki <lukasz.sawinski2002@gmail.com>
2024-05-30 00:24:35 +0200

    Add new files

---
+++ ProjektSolo/Plyta.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektSolo
+{
+    public struct Plyta
+    {
+        public string tytul;
+        public string typ;
+        public string czasTrwania;
+        public List<Utwor> utwory;
+        public string wykonawcy;
+        public int id;
+
+        public Plyta(string tytulPlyty, string typPlyty, string czasTrwaniaPlyty, List<Utwor> utworyPlyty, string wykonawcyPlyty, int idPlyty)
+        {
+            tytul = tytulPlyty;
+            typ = typPlyty;
+            czasTrwania = czasTrwaniaPlyty;
+            utwory = utworyPlyty;
+            wykonawcy = wykonawcyPlyty;
+            id = idPlyty;
+        }
+
+        public struct Utwor
+        {
+            public string tytul;
+            public string czasTrwania;
+            public string wykonawcy;
+            public string kompozytor;
+            public int id;
+
+            public Utwor(string tytulUtworu, string czasTrwaniaUtworu, string wykonawcyUtworu, string kompozytorUtworu, int idUtworu)
+            {
+                tytul = tytulUtworu;
+                czasTrwania = czasTrwaniaUtworu;
+                wykonawcy = wykonawcyUtworu;
+                kompozytor = kompozytorUtworu;
+                id = idUtworu;
+            }
+        }
+
+        public class UlubionyWykonawcaEver
+        {
+            public string imie;
+            public string nazwisko;
+            public string gatunekMuzyczny;
+            public string krajPochodzenia;
+
+            public UlubionyWykonawcaEver(string imieWyk, string nazwiskoWyk, string gatunekMuzycznyWyk, string krajPochodzeniaWyk = "Polska")
+            {
+                imie = imieWyk;
+                nazwisko = nazwiskoWyk;
+                gatunekMuzyczny = gatunekMuzycznyWyk;
+                krajPochodzenia = krajPochodzeniaWyk;
+            }
+        }
+
+        public class UlubionyUtworT
+        {
+            public string tytul;
+            public string wykonawca;
+            public string gatunek;
+            public int rokWydania;
+
+            public UlubionyUtworT(string tytulU, string wykonawcaU, string gatunekU, int rokWydaniaU = 2024)
+            {
+                tytul = tytulU;
+                wykonawca = wykonawcaU;
+                gatunek = gatunekU;
+                rokWydania = rokWydaniaU;
+            }
+        }
+    }
+}
